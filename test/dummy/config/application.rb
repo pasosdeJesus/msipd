e7aed403266656260184075db28cae15bac763c1
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2021-02-08 20:18:40 -0500

    Actualiza

--- test/dummy/config/application.rb
+++ test/dummy/config/application.rb
@@ -2,27 +2,46 @@ require_relative 'boot'
 
 require 'rails/all'
 
+# Requiere gemas listas en el Gemfile, incluyendo las
+# limitadas a :test, :development, o :production.
 Bundler.require(*Rails.groups)
-require "sipd"
-
 
 module Dummy
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 6.0
 
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
+    # config.load_defaults 6.0
+
+    # Las configuraciones en config/environments/* tiene precedencia sobre
+    # las especificadas aquí.
+    # La configuración de la aplicación puede ir en archivos en
+    # config/initializers
+    # -- todos los archivos .rb en ese directorio se cargan automáticamente
+    # tras cargar el entorno y cualquier gema en su aplicación.
+    
+    # Establece Time.zone por defecto en la zona especificada y hace que
+    # Active Record auto-convierta a esta zona.
+    # Ejecute "rake -D time" para ver una lista de tareas para encontrar
+    # nombres de zonas. Por omisión es UTC.
     config.time_zone = 'America/Bogota'
-    config.i18n.default_locale = :es
-    config.x.formato_fecha = 'dd/M/yyyy'
-    config.active_record.schema_format = :sql
-    config.railties_order = [:main_app, Sip::Engine, :all]
 
-    config.hosts << ENV['CONFIG_HOSTS'] || '127.0.0.1'
+    # El locale predeterminado es :en y todas las traducciones de
+    # config/locales/*.rb,yml se cargan automaticamente
+    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
+    config.i18n.default_locale = :es
+
+    config.colorize_logging = true
+
+    config.active_record.schema_format = :sql
+
+    config.hosts << ENV.fetch('CONFIG_HOSTS', '127.0.0.1')
+
+    #config.web_console.whitelisted_ips = ['186.154.35.237']
+    
+    # config.relative_url_root = ENV.fetch('RUTA_RELATIVA', '/heb412')
+
+    # sip
+    config.x.formato_fecha = ENV.fetch('SIP_FORMATO_FECHA', 'dd/M/yyyy')
+    # En el momento soporta 3 formatos: yyyy-mm-dd, dd-mm-yyyy y dd/M/yyyy
 
   end
 end
-
