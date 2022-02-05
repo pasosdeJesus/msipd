e2eb29322b27ecdfbdde47aae48bbab23cfe9459
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2022-02-05 06:48:11 -0500

    renombra

--- app/models/sipd/dominio_operaen_pais.rb
+++ app/models/sipd/dominio_operaen_pais.rb
@@ -1,7 +1,7 @@
 require 'sipd/concerns/models/dominio_operaen_pais'
 
 module Sipd
-  class DominioOperanePais < ActiveRecord::Base
+  class DominioOperaenPais < ActiveRecord::Base
     include Sipd::Concerns::Models::DominioOperaenPais
   end
 end
