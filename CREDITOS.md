ff8690af0c50b085a894ca49587a8dbac1bd8390
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2022-06-03 11:49:31 -0500

    Mejora creditos y acerca de

---
+++ CREDITOS.md
@@ -0,0 +1,14 @@
+Puede ver el listado de personas que han implementado en:
+<https://github.com/pasosdeJesus/sipd/graphs/contributors>
+
+Este motor amplia el motor sip, por favor revise los créditos 
+de ese motor en
+<https://github.com/pasosdeJesus/sip/blob/master/CREDITOS.md>
+
+
+Agradecemos a Jesús/Dios por el poder que nos ha dado:
+
+  Porque no nos ha dado Dios espíritu de cobardía
+  sino de poder, de amor y de dominio propio.
+
+  2 Timoteo 1:7
--- test/dummy/config/initializers/sip.rb
+++ test/dummy/config/initializers/sip.rb
@@ -6,5 +6,21 @@ Sip.setup do |config|
   config.ruta_volcados = ENV.fetch('SIP_RUTA_VOLCADOS',
                                    "#{Rails.root}/archivos/bd")
   config.titulo = "sipd #{Sipd::VERSION}"
+  config.descripcion = "Motor que extiende sip con dominios"
+  config.codigofuente = "https://github.com/pasosdeJesus/sipd"
+  config.urlcontribuyentes = "https://github.com/pasosdeJesus/sipd/graphs/contributors"
+  config.urlcreditos = "https://github.com/pasosdeJesus/sipd/blob/master/CREDITOS.md"
+  config.agradecimientoDios = "<p>
+Agradecemos a Jesús/Dios por el poder que nos ha dado:
+</p>
+<blockquote>
+  <p>
+  Porque no nos ha dado Dios espíritu de cobardía
+  sino de poder, de amor y de dominio propio.
+  </p><p>
+  2 Timoteo 1:7
+  </p>
+</blockquote>".html_safe
+
   config.longitud_nusuario = 10
 end
