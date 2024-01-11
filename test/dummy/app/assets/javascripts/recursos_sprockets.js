2f5f0d71b8580a462cd60cd1cc3f074374b28d8d
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2024-01-11 15:46:45 -0500

    Actualiza

--- test/dummy/app/assets/javascripts/recursos_sprockets.js
+++ test/dummy/app/assets/javascripts/recursos_sprockets.js
@@ -10,6 +10,5 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require activestorage
 //= require msip/application
 //= require_tree .
